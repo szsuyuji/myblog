3f6f1f86bc45d04c91ce8773c17a66ff8c5816ab
syj <szsuyuji@gmail.com>
2014-12-05 08:42:03 +0800

    修改资源库实现位置

--- misite/src/main/java/com/szsuyuji/blog/domain/model/repository/TopicHibernateRepository.java
+++ misite/src/main/java/com/szsuyuji/blog/domain/model/repository/TopicHibernateRepository.java
@@ -1,8 +1,9 @@
-package com.szsuyuji.blog.infrastructure.persistence;
+package com.szsuyuji.blog.domain.model.repository;
 
 import org.springframework.stereotype.Repository;
 
 import com.szsuyuji.blog.domain.model.Topic;
+import com.szsuyuji.blog.infrastructure.persistence.HibernateRepository;
 
 @Repository
 public class TopicHibernateRepository extends HibernateRepository<Topic, Long> {
